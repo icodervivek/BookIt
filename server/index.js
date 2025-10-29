d311c9bce35223102bf6d3ad8346111db0d6c993
icodervivek <vivekpramanikbundu@gmail.com>
2025-10-29 07:47:14 +0530

    feat: add env variable in index

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ app.get("/", (req, res) => {
 });
 
 if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT;
+  const PORT = process.env.PORT || 5000;
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
